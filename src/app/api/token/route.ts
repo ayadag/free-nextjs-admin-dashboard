542c91af0cb7126fd924a2c8910cbf00244e8b81
96ayad <96ayad@gmail.com>
2024-07-17 19:36:12 +0300

    api

---
+++ src/app/api/token/route.ts
@@ -0,0 +1,89 @@
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+
+import { fetchDigitalAsset } from '@metaplex-foundation/mpl-token-metadata';
+import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
+import { fromWeb3JsPublicKey } from '@metaplex-foundation/umi-web3js-adapters';
+import { PublicKey } from '@solana/web3.js';
+
+export async function GET(req: NextRequest, res: NextResponse) {
+  if (req.method === 'GET') {
+
+    try {
+      const searchParams = await req.nextUrl.searchParams;
+
+      let tokenId = searchParams.get('tokenId');
+      tokenId = String(tokenId);
+
+      if(!tokenId){
+        return new NextResponse(JSON.stringify({ success: false, error: '!tokenId)'}), {
+                  status: 500,
+                  headers: {
+                    'Content-Type': 'application/json',
+                    // 'Access-Control-Allow-Origin': '*',
+                  },
+        });
+      }
+
+      // const tokenM = await Token(tokenId);
+      const tokenM = await Token('Duqm5K5U1H8KfsSqwyWwWNWY5TLB9WseqNEAQMhS78hb');
+      const data = tokenM;
+
+      return new NextResponse(JSON.stringify({ success: true, data }), {
+          status: 200,
+          headers: {
+            'Content-Type': 'application/json',
+            // 'Access-Control-Allow-Origin': '*',
+          },
+      });
+
+    
+    } catch (error) {
+      console.log("Error: ", error)
+      return new NextResponse(JSON.stringify({ success: false, error: (error as Error).message as string }), {
+        status: 500,
+        headers: {
+          'Content-Type': 'application/json',
+          // 'Access-Control-Allow-Origin': '*',
+        },
+      });
+    }
+  } else {
+    return new NextResponse('Method Not Allowed', {
+      status: 405,
+      headers: {
+        'Allow': 'GET',
+      },
+    });
+  }
+}
+
+
+async function Token(tokenId: string) {
+  // const token = new web3.PublicKey('So11111111111111111111111111111111111111112'); //SOL
+  // const token = new web3.PublicKey('Duqm5K5U1H8KfsSqwyWwWNWY5TLB9WseqNEAQMhS78hb'); //SALD
+  const token = new PublicKey(tokenId); //SALD
+  // const umi = createUmi('https://api.devnet.solana.com', 'processed')
+  // Use the RPC endpoint of your choice.
+  // const umi = createUmi('https://api.devnet.solana.com').use(mplTokenMetadata())
+  const umi = createUmi('https://api.devnet.solana.com')
+  const mint = fromWeb3JsPublicKey(token);
+  const asset = await fetchDigitalAsset(umi, mint) 
+  // console.log('asset: ', asset);
+  const supply = Number(asset.mint.supply)/1000000000;  //1=1000000000n
+
+  const tokenData = {
+    publicKey: asset.publicKey,
+    owner: asset.mint.header.owner,
+    mintAuthority: asset.mint.mintAuthority,
+    updateAuthority: asset.metadata.updateAuthority,
+    name: asset.metadata.name,
+    symbol: asset.metadata.symbol,
+    uri: asset.metadata.uri,
+    decimals: asset.mint.decimals,
+    supply: supply,
+    executable: asset.mint.header.executable,
+  }
+  // console.log('tokenData: ', tokenData)
+  return tokenData
+}
--- src/app/pool/pools/page.tsx
+++ src/app/pool/pools/page.tsx
@@ -1,13 +1,17 @@
+
 "use client"
 import React, { useEffect } from 'react';
 
+import { useParams } from 'next/navigation';
+
 function Pools() {
+    const path = useParams
     useEffect(() => {
         getPools();
     })
 
     const getPools = async () => {
-        const url = 'https://serverless-jizen7j8e-ayads-projects.vercel.app';
+        const url = 'https://serverless-fy6j77er0-ayads-projects.vercel.app';
         const searchParam = {
             page: 1,
             perPage: 3,
@@ -17,9 +21,38 @@ function Pools() {
             const pools = await ( await fetch(
                 // `${url}/pools?page=${searchParam.page}&perPage=${searchParam.perPage}`
                 // `${url}/api/pools?page=${searchParam.page}&perPage=${searchParam.perPage}`
-                `https://serverless-jizen7j8e-ayads-projects.vercel.app/api/pools?page=1&perPage=3`    
+                // `http://localhost:3000/api/pools?page=1&perPage=3`  
+                // `https://serverless-qyjftt2jf-ayads-projects.vercel.app/api/pools?page=1&perPage=3` //https://serverless-qyjftt2jf-ayads-projects.vercel.app
+                // `http://localhost:3000/api/pools?page=1&perPage=3`
+                `/api/pools?page=1&perPage=3`
             )
             ).json();
+//               //Create fetch's options
+//   const options = {
+//     mode: 'cors',
+//     method: 'GET',
+//     credentials: 'include',
+//     headers: {
+//        'Content-Type': 'application/json',
+//        Origin: `https://google.com/`,
+//     }
+//  }
+
+//  //Fetch data from the API
+//  const res = await fetch('changeForYouURL', options);
+
+            // const pools = await ( await fetch(
+            //     // `${url}/pools?page=${searchParam.page}&perPage=${searchParam.perPage}`
+            //     // `${url}/api/pools?page=${searchParam.page}&perPage=${searchParam.perPage}`
+            //     `https://serverless-fy6j77er0-ayads-projects.vercel.app/api/pools?page=1&perPage=3`  
+            //     , {mode: 'cors', method: 'GET', 
+            //         headers: {
+            //                'Content-Type': 'application/json',
+            //                Origin: `http://localhost:3000`,
+            //         }
+            //     }
+            // )
+            // ).json();
 
             console.log(pools)
         } catch (error) {
