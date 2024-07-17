8a57749b1d566d4b60aea9fc9b1f17f404d9e665
96ayad <96ayad@gmail.com>
2024-07-17 22:55:44 +0300

    toFixed

--- src/app/pool/pools/page.tsx
+++ src/app/pool/pools/page.tsx
@@ -1,107 +1,24 @@
+import { Metadata } from 'next';
 
-"use client"
-import React, { useEffect } from 'react';
+import Breadcrumb from '@/components/Breadcrumbs/Breadcrumb';
+import DefaultLayout from '@/components/Layouts/DefaultLayout';
+import PoolsC from '@/components/pool/pools';
 
-import { useParams } from 'next/navigation';
+export const metadata: Metadata = {
+  title: "Next.js Tables | TailAdmin - Next.js Dashboard Template",
+  description:
+    "This is Next.js Tables page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
+};
 
-function Pools() {
-    const path = useParams
-    useEffect(() => {
-        getPools();
-    })
+const Pools = () => {
+  return (
+    <DefaultLayout>
+      <Breadcrumb pageName="Pools" />
 
-    const getPools = async () => {
-        const url = 'https://serverless-fy6j77er0-ayads-projects.vercel.app';
-        const searchParam = {
-            page: 1,
-            perPage: 3,
-        }
-    
-        try {
-            const pools = await ( await fetch(
-                // `${url}/pools?page=${searchParam.page}&perPage=${searchParam.perPage}`
-                // `${url}/api/pools?page=${searchParam.page}&perPage=${searchParam.perPage}`
-                // `http://localhost:3000/api/pools?page=1&perPage=3`  
-                // `https://serverless-qyjftt2jf-ayads-projects.vercel.app/api/pools?page=1&perPage=3` //https://serverless-qyjftt2jf-ayads-projects.vercel.app
-                // `http://localhost:3000/api/pools?page=1&perPage=3`
-                `/api/pools?page=1&perPage=3`
-            )
-            ).json();
-//               //Create fetch's options
-//   const options = {
-//     mode: 'cors',
-//     method: 'GET',
-//     credentials: 'include',
-//     headers: {
-//        'Content-Type': 'application/json',
-//        Origin: `https://google.com/`,
-//     }
-//  }
-
-//  //Fetch data from the API
-//  const res = await fetch('changeForYouURL', options);
-
-            // const pools = await ( await fetch(
-            //     // `${url}/pools?page=${searchParam.page}&perPage=${searchParam.perPage}`
-            //     // `${url}/api/pools?page=${searchParam.page}&perPage=${searchParam.perPage}`
-            //     `https://serverless-fy6j77er0-ayads-projects.vercel.app/api/pools?page=1&perPage=3`  
-            //     , {mode: 'cors', method: 'GET', 
-            //         headers: {
-            //                'Content-Type': 'application/json',
-            //                Origin: `http://localhost:3000`,
-            //         }
-            //     }
-            // )
-            // ).json();
-
-            console.log(pools)
-        } catch (error) {
-            console.log('error', error)
-        }
-        // const pools = await ( await fetch(
-        //     // `${url}/pools?page=${searchParam.page}&perPage=${searchParam.perPage}`
-        //     // `${url}/api/pools?page=${searchParam.page}&perPage=${searchParam.perPage}`
-        //     `https://serverless-278sktyk2-ayads-projects.vercel.app/api/pools?page=1&perPage=3`    
-        // )
-        // ).json();
-    
-        // const pools = await fetch(
-        //     // `${url}/pools?page=${searchParam.page}&perPage=${searchParam.perPage}`
-        //     // `${url}/api/pools?page=${searchParam.page}&perPage=${searchParam.perPage}`
-        //     `https://serverless-278sktyk2-ayads-projects.vercel.app/api/pools?page=1&perPage=3`    
-        // )
-    
-        // const pools = await( await fetch('https:serverless-278sktyk2-ayads-projects.vercel.app/api/pools?page=1&perPage=3', {
-        //     headers:{
-        //       accept: 'application/json',
-        //       'User-agent': 'learning app',
-        //     }
-        //   })).json();
-    
-        // const pools = fetch(
-        //     // `${url}/pools?page=${searchParam.page}&perPage=${searchParam.perPage}`
-        //     // `${url}/api/pools?page=${searchParam.page}&perPage=${searchParam.perPage}`
-        //     `https://serverless-278sktyk2-ayads-projects.vercel.app/api/pools?page=1&perPage=3`    
-        // )
-        // // .then(res => console.log(res))
-        // .then(function(serverPromise){ 
-        //     serverPromise.json()
-        //       .then(function(j) { 
-        //         console.log(j); 
-        //       })
-        //       .catch(function(e){
-        //         console.log(e);
-        //       });
-        // })
-    
-        // .catch(function(e){
-        //       console.log(e);
-        // });
-    
-        // console.log(pools)
-    }
-    return (
-        <h1>Hello world</h1>
-    )
+      <div className="flex flex-col gap-10">
+        <PoolsC />
+      </div>
+    </DefaultLayout>
+  );
 }
 export default Pools;
---
+++ src/components/pool/pools.tsx
@@ -0,0 +1,158 @@
+"use client"
+import React, { useEffect } from 'react';
+
+// import { BRAND } from "@/types/brand";
+import Image from 'next/image';
+
+import poolsL from './poolsList.json';
+
+const poolsList: any[] = poolsL.data
+const logo = "/images/brand/brand-01.svg"
+
+// const brandData: BRAND[] = [
+const brandData = [
+        {
+    logo: "/images/brand/brand-01.svg",
+    name: "Google",
+    visitors: 3.5,
+    revenues: "5,768",
+    sales: 590,
+    conversion: 4.8,
+  },
+  {
+    logo: "/images/brand/brand-02.svg",
+    name: "Twitter",
+    visitors: 2.2,
+    revenues: "4,635",
+    sales: 467,
+    conversion: 4.3,
+  },
+  {
+    logo: "/images/brand/brand-03.svg",
+    name: "Github",
+    visitors: 2.1,
+    revenues: "4,290",
+    sales: 420,
+    conversion: 3.7,
+  },
+  {
+    logo: "/images/brand/brand-04.svg",
+    name: "Vimeo",
+    visitors: 1.5,
+    revenues: "3,580",
+    sales: 389,
+    conversion: 2.5,
+  },
+  {
+    logo: "/images/brand/brand-05.svg",
+    name: "Facebook",
+    visitors: 3.5,
+    revenues: "6,768",
+    sales: 390,
+    conversion: 4.2,
+  },
+];
+
+const PoolsC = () => {
+    //Get pools
+    useEffect(() => {
+        getPools();
+    })
+
+    const getPools = async () => {
+        // const url = 'https://serverless-fy6j77er0-ayads-projects.vercel.app';       
+        const searchParam = {
+            page: 1,
+            perPage: 10,
+        }
+        const url = `/api/pools?page=${searchParam.page}&perPage=${searchParam.perPage}`;
+
+        try {
+            const pools = await ( await fetch(
+                `${url}`
+                )
+            ).json();
+
+            console.log(pools)
+        } catch (error) {
+            console.log('error', error)
+        }
+    }
+
+  return (
+    <div className="rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
+      <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">
+        Pools List
+      </h4>
+
+      <div className="flex flex-col">
+        <div className="grid grid-cols-3 rounded-sm bg-gray-2 dark:bg-meta-4 sm:grid-cols-5">
+          <div className="p-2.5 xl:p-5">
+            <h5 className="text-sm font-medium uppercase xsm:text-base">
+              Pair
+            </h5>
+          </div>
+          <div className="p-2.5 text-center xl:p-5">
+            <h5 className="text-sm font-medium uppercase xsm:text-base">
+              Pool
+            </h5>
+          </div>
+          <div className="p-2.5 text-center xl:p-5">
+            <h5 className="text-sm font-medium uppercase xsm:text-base">
+              Mint A
+            </h5>
+          </div>
+          <div className="hidden p-2.5 text-center sm:block xl:p-5">
+            <h5 className="text-sm font-medium uppercase xsm:text-base">
+              Mint B
+            </h5>
+          </div>
+          <div className="hidden p-2.5 text-center sm:block xl:p-5">
+            <h5 className="text-sm font-medium uppercase xsm:text-base">
+              Pool Price
+            </h5>
+          </div>
+        </div>
+
+        {poolsList.map((pool, key) => (
+          <div
+            className={`grid grid-cols-3 sm:grid-cols-5 ${
+              key === brandData.length - 1
+                ? ""
+                : "border-b border-stroke dark:border-strokedark"
+            }`}
+            key={key}
+          >
+            <div className="flex items-center gap-3 p-2.5 xl:p-5">
+              <div className="flex-shrink-0">
+                <Image src={logo} alt="Brand" width={48} height={48} />
+              </div>
+              <p className="hidden text-black dark:text-white sm:block">
+                {/* {brand.name} */}
+                tokenA - tokenB
+              </p>
+            </div>
+
+            <div className="flex items-center justify-center p-2.5 xl:p-5">
+              <p className="text-black dark:text-white">{pool.poolId.substring(0, 4)} ...</p>
+            </div>
+
+            <div className="flex items-center justify-center p-2.5 xl:p-5">
+              <p className="text-meta-3">{pool.mintA.substring(0, 4)} ...</p>
+            </div>
+
+            <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
+              <p className="text-black dark:text-white">{pool.mintB.substring(0, 4)} ...</p>
+            </div>
+
+            <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
+              <p className="text-meta-5">{Number(Math.floor(pool.poolPrice * 10000).toFixed(5))/10000}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PoolsC;
