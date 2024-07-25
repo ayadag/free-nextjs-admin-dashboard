6a01ecda12b8342888cc5705e717c0392685ad0a
96ayad <96ayad@gmail.com>
2024-07-25 11:14:26 +0300

    payer

--- src/components/tokenv2/withdraw/withdraw.tsx
+++ src/components/tokenv2/withdraw/withdraw.tsx
@@ -1,4 +1,3 @@
-"use client";
 import React, {
   useEffect,
   useRef,
@@ -6,10 +5,14 @@ import React, {
 } from 'react';
 
 import { message as Message } from 'antd';
+import bs58 from 'bs58';
 
 import {
   getAssociatedTokenAddress,
+  getTransferFeeAmount,
   TOKEN_2022_PROGRAM_ID,
+  unpackAccount,
+  withdrawWithheldTokensFromAccounts,
 } from '@solana/spl-token';
 // import {
 //   Raydium,
@@ -23,7 +26,10 @@ import {
   useConnection,
   useWallet,
 } from '@solana/wallet-adapter-react';
-import { PublicKey } from '@solana/web3.js';
+import {
+  Keypair,
+  PublicKey,
+} from '@solana/web3.js';
 
 import Error from '../../alert/error';
 // import { notify } from '../../utils/notifications';
@@ -32,6 +38,8 @@ import {
   getTransaction,
   TxDetail,
 } from './getTxDetails';
+
+"use client";
 
 // import styles from './swap.module.css';
 // import tList from './tokenList2.json';
@@ -45,7 +53,7 @@ import {
 const WithdrawC: React.FC = () => {
     const wallet = useWallet();
     const { publicKey, signAllTransactions } = useWallet();
-    
+
     // const connection = new Connection('https://mainnet.helius-rpc.com/?api-key=YOUR_API_KEY_HERE');
     // const connection = new Connection('https://api.devnet.solana.com/');
     const { connection } = useConnection()
@@ -120,7 +128,7 @@ const WithdrawC: React.FC = () => {
             "programId": "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA",
         }
     );
-    
+
 
     //ayad/////////////////////////////////////////////////////////////
     // const [mint, setMint] = useState<string | undefined>('');
@@ -128,15 +136,15 @@ const WithdrawC: React.FC = () => {
     const [tokenOwner, setTokenOwner] = useState<string | undefined>(owner?.toBase58())
     const [ata, setAta] = useState('');
     const [withheld, setWithheld] = useState('');
-    
+
     //Handle mint changes
     useEffect(() => {
-        if(!mint || !tokenOwner) {return console.log('!mint || !tokenOwner')}
+        if (!mint || !tokenOwner) { return console.log('!mint || !tokenOwner') }
 
         getATADetails();
 
         async function getATADetails() {
-            if(!mint || !tokenOwner) {return console.log('!mint || !tokenOwner')}
+            if (!mint || !tokenOwner) { return console.log('!mint || !tokenOwner') }
             const ATAdress = await getAssociatedTokenAddress(
                 new PublicKey(mint),
                 // payer.publicKey,
@@ -150,7 +158,7 @@ const WithdrawC: React.FC = () => {
             console.log('ATAdress.toBase58(): ', ATAdress.toBase58())
             console.log('setAta: ', ata)
         }
-    },[mint])
+    }, [mint])
 
 
     //scroll effect whenever the message change
@@ -159,7 +167,7 @@ const WithdrawC: React.FC = () => {
     }, [error, txDetails]);
 
     //Show Successful message
-    const showSuccessfulMessage= () => {
+    const showSuccessfulMessage = () => {
         // window.scroll({
         //   top:0, //scroll to the top of page
         //   behavior: 'smooth'
@@ -168,12 +176,12 @@ const WithdrawC: React.FC = () => {
         setShowPoolId(true) //show pool id
         // messageRef.current?.scrollIntoView({ behavior: 'smooth' });
         setTimeout(() => {
-          setSuccessful(false); //hide successful message after 10s
+            setSuccessful(false); //hide successful message after 10s
         }, 10000);
     };
-    
+
     //Show Error message
-    const showErrorMessage= () => {
+    const showErrorMessage = () => {
         // window.scrollTo({
         //   top:0, //scroll to the top of page
         //   behavior: 'smooth'
@@ -183,38 +191,38 @@ const WithdrawC: React.FC = () => {
         // window.scrollTo(0,0) //scroll to x=0 y=0
         // router.push(`/tokenv1/create`)
         setTimeout(() => {
-          setError(false); //hide Error message after 10s
+            setError(false); //hide Error message after 10s
         }, 10000);
     };
 
     //Handle txId changes
-    useEffect( ()=> {
+    useEffect(() => {
         // try {
-           if(!txId){return console.log('!txId')}
+        if (!txId) { return console.log('!txId') }
         //    get();
-           setTimeout(get, 500) //call get function after 0.5 second
-            // } catch (err) {console.log(err)}
-    },[txId])
+        setTimeout(get, 500) //call get function after 0.5 second
+        // } catch (err) {console.log(err)}
+    }, [txId])
 
     //Handle txDetails changes
     useEffect(() => {
-        if(!txDetails) {return console.log('!txDetails')}
+        if (!txDetails) { return console.log('!txDetails') }
         console.log('txDetails: ', txDetails);
         setMessage(txDetails.message) //transaction message
-        if(txDetails.state == 'success'){
+        if (txDetails.state == 'success') {
             showSuccessfulMessage(); //show successful message for 10 seccond
         }
-        else if(txDetails.state == 'error'){
+        else if (txDetails.state == 'error') {
             showErrorMessage(); //show successful message for 10 seccond
         }
-    },[txDetails])
+    }, [txDetails])
 
 
 
     //Get transaction details
     async function get() {
         const txD = await getTransaction(connection, txId)
-        if(!txD) {return console.log('!txD')}
+        if (!txD) { return console.log('!txD') }
         setTxDetails(txD)
     }
 
@@ -225,7 +233,73 @@ const WithdrawC: React.FC = () => {
     }
 
     async function withdrawF() {
-        
+        if (!publicKey || !mint) { return console.log('!publicKey') }
+        const balance = await connection.getBalance(publicKey);
+        if (balance < 10000000) { // 0.01 SOL
+            return console.error('Not enough SOL in payer account, please fund your waalet');
+        }
+
+        const allAccounts = await connection.getProgramAccounts(TOKEN_2022_PROGRAM_ID, {
+            commitment: 'confirmed',
+            filters: [
+                {
+                    memcmp: {
+                        offset: 0,
+                        //   bytes: mint.toString(),
+                        bytes: mint,
+                    },
+                },
+            ],
+        });
+
+        const accountsToWithdrawFrom = [];
+
+        for (const accountInfo of allAccounts) {
+            const account = unpackAccount(
+                accountInfo.pubkey,
+                accountInfo.account,
+                TOKEN_2022_PROGRAM_ID
+            );
+
+            // We then extract the transfer fee extension data from the account
+            const transferFeeAmount = getTransferFeeAmount(account);
+
+            if (
+                transferFeeAmount !== null &&
+                transferFeeAmount.withheldAmount > BigInt(0)
+            ) {
+                accountsToWithdrawFrom.push(accountInfo.pubkey);
+            }
+        }
+
+        if (accountsToWithdrawFrom.length === 0) {
+            return console.error('No accounts to withdraw from: no transfers have been made');
+        } else {
+            console.log('Found', accountsToWithdrawFrom.length, 'accounts to withdraw from 🤑');
+            console.log('accountsToWithdrawFrom: ', accountsToWithdrawFrom) //ayad
+        }
+
+        const withdrawWithheldAuthority = Keypair.fromSecretKey(
+            // new Uint8Array(withheld)
+            new Uint8Array(bs58.decode(withheld))
+        );
+        const withdrawTokensSig = await withdrawWithheldTokensFromAccounts(
+            connection, // connection to use
+            wallet, // payer of the transaction fee
+            new PublicKey(mint), // the token mint
+            // recipientKeypair.publicKey, // the destination account
+            new PublicKey(ata),  // the destination account
+            withdrawWithheldAuthority, // the withdraw withheld token authority
+            [], // signing accounts
+            accountsToWithdrawFrom, // source accounts from which to withdraw withheld fees
+            undefined, // options for confirming the transaction
+            TOKEN_2022_PROGRAM_ID // SPL token program id
+        );
+
+        console.log(
+            'Bag secured, check it:',
+            `https://solana.fm/tx/${withdrawTokensSig}?cluster=devnet-solana`
+        );
     }
 
     return (
@@ -234,7 +308,7 @@ const WithdrawC: React.FC = () => {
             {/* <div ref={messageRef}></div>  */}
             {successful && Successful(message, txId)} {/*sccessful message*/}
             {/* {error && Error(message, details, messageRef)} error message */}
-            {error && Error(message, undefined, txId)} 
+            {error && Error(message, undefined, txId)}
 
             {/* <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark"> */}
             <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
@@ -279,7 +353,7 @@ const WithdrawC: React.FC = () => {
                                             placeholder="Enter token owner address"
                                             // step="0.000000001"
                                             onChange={(e) => setTokenOwner(String(e.target.value))}
-                                            // defaultValue={tokenOneAmount}
+                                            defaultValue={tokenOwner}
                                             required
                                             className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                                         />
@@ -296,7 +370,7 @@ const WithdrawC: React.FC = () => {
                                             placeholder="Enter recipient address"
                                             // step="0.000000001"
                                             onChange={(e) => setAta(String(e.target.value))}
-                                            // defaultValue={tokenTwoAmount}
+                                            defaultValue={ata}
                                             required
                                             className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                                         />
@@ -328,7 +402,7 @@ const WithdrawC: React.FC = () => {
                                     />
                                 </div>
 
-                                {showPoolId &&<div className="mt-6 text-center">
+                                {showPoolId && <div className="mt-6 text-center">
                                     {/* <p className='text-white'>Created Pool Id:</p> */}
                                     <p className='text-white'>
                                         {`Created pool id: ${poolId}`}
