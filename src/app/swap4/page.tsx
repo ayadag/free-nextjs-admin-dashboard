8ae40db060cd105ecc99ab8451508ae021b426e4
ayadag <ayad.ali.g@gmail.com>
2024-07-26 10:59:49 +0300

    allowFullScreen

--- src/app/swap4/page.tsx
+++ src/app/swap4/page.tsx
@@ -745,8 +745,8 @@ import tList from './tokenList2.json';
                     width="100%"
                     height="600"
                     src="https://birdeye.so/tv-widget/DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263?chain=solana&viewMode=pair&chartInterval=1D&chartType=CANDLE&chartTimezone=Asia%2FSingapore&chartLeftToolbar=show&theme=dark"
-                    frameborder="0"
-                    allowfullscreen
+                    frameBorder="0"
+                    allowFullScreen
                   ></iframe>
                   </div>
                  {/* </div> */}
