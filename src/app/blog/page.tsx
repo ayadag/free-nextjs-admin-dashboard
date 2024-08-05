4a8224728fe09e06c0520a2a4c17a29bcc8ec790
96ayad <96ayad@gmail.com>
2024-08-05 13:36:12 +0300

    about

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -42,7 +42,8 @@ const AriclePage = async () => {
   const articles = await getAllArticles();
   return (
     <SimpleLayout
-      title="Writing on software design, company building, and the aerospace industry."
+      // title="Writing on software design, company building, and the aerospace industry."
+      title="Writing on crypto currency and decentraized exchange platforms."
       intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
     >
       <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
